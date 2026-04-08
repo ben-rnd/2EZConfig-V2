c398dd493e3395067cc15283ec3edbbb616268ee
EZ2Config Dev <kasaski97@gmail.com>
2026-04-08 23:16:00 +1000

    Fixed 5th trax wrong id

--- src/common/include/game_defs.h
+++ src/common/include/game_defs.h
@@ -19,7 +19,7 @@ static Game games[] = {
     {"2nd Trax ~It rules once again~",          "EZ2DJ.exe",          "ez2dj_2nd",    GameFamily::EZ2DJ},
     {"3rd Trax ~Absolute Pitch~",               "EZ2DJ.exe",          "ez2dj_3rd",    GameFamily::EZ2DJ},
     {"4th Trax ~OVER MIND~",                    "EZ2DJ.exe",          "ez2dj_4th",    GameFamily::EZ2DJ},
-    {"Platinum",                                "EZ2DJ.exe",          "ez2dj_pt",     GameFamily::EZ2DJ},
+    {"Platinum",                                "EZ2DJ.exe",          "ez2dj_5th",     GameFamily::EZ2DJ},
     {"6th Trax ~Self Evolution~",               "EZ2DJ-Launcher.exe", "ez2dj_6th",    GameFamily::EZ2DJ},
     {"7th Trax ~Resistance~",                   "EZ2DJ.exe",          "ez2dj_7th",    GameFamily::EZ2DJ},
     {"7th Trax Ver 1.5",                        "EZ2DJ.exe",          "ez2dj_7th_15", GameFamily::EZ2DJ},
